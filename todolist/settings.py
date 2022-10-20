1e073c42f4673db0f3f9d6f0cebb3aea3ec01f70
peer <pavel.shaura@mail.ru>
2022-10-20 23:04:28 +0300

    fix pagination to trtial usages #002

--- todolist/settings.py
+++ todolist/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 50,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
